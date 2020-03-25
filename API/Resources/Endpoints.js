a455303e83dfbde805243385b831cf68e4e6ea27
FortTellDevelop <61522303+FortTellDevelop@users.noreply.github.com>
2020-03-25 19:55:01 +0000

    Create Endpoints.js

---
+++ API/Resources/Endpoints.js
@@ -0,0 +1,27 @@
+module.exports = {
+    
+    /* Auth Endpoints */
+    Auth: 'https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token',
+
+    Exchange: 'https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/exchange',
+
+    Verify: 'https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/verify?includePerms=true',
+
+    /* Fortnite Endpoints */
+    PVEInfo: 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/world/info',
+
+    Store: 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/storefront/v2/catalog',
+
+    Status: 'https://lightswitch-public-service-prod06.ol.epicgames.com/lightswitch/api/service/bulk/status?serviceId=Fortnite',
+
+    News: 'https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game',
+
+    EventFlag: 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/calendar/v1/timeline',
+
+    /* Launcher Endpoints */
+    KeyChain: 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/storefront/v2/keychain',
+
+    CloudStorage: 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/cloudstorage/system',
+
+    Manifest: 'https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/public/assets/v2/platform/Windows/namespace/fn/catalogItem/4fe75bbc5a674f4f9b356b5c90567da5/app/Fortnite/label/Live'
+};
